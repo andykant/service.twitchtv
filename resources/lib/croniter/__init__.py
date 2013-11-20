30e86df9b13810d84372bee06aaae07f36678844
Andy Kant <andy@andykant.com>
2013-11-20 01:01:57 -0600

    Added croniter lib

---
+++ resources/lib/croniter/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# defer imports to be accesible in setup.py
+
+from __future__ import absolute_import
+
+from ._release import (
+    __doc__,
+    __author__,
+    __version__,
+    __license__,
+)
+from .croniter import croniter
